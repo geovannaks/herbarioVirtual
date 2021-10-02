5fdcfad975a32c3d75b7f9f007234489d1ad0ed8
geovannaks <geovanna.kasemirinski@gmail.com>
2021-10-02 20:15:22 -0300

    Criação da cell e search bar no storyboard

---
+++ herbarioVirtual/PlantTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PlantTableViewCell.swift
+//  herbarioVirtual
+//
+//  Created by Geovanna Kasemirinski da Silva on 02/10/21.
+//
+
+import UIKit
+
+class PlantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
