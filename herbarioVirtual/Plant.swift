d8359e513ee8ab005f561222120868bf774d77fb
geovannaks <geovanna.kasemirinski@gmail.com>
2021-10-03 17:22:03 -0300

    Variáveis que serão usada

---
+++ herbarioVirtual/Plant.swift
@@ -0,0 +1,15 @@
+//
+//  Plant.swift
+//  herbarioVirtual
+//
+//  Created by Geovanna Kasemirinski da Silva on 03/10/21.
+//
+
+import Foundation
+import UIKit
+
+struct Plant{
+    let name: String
+    let image: UIImage
+    
+}
